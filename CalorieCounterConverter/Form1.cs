a9f294f35fdc9ad9a09910dd3180f1ff4e1aba4e
The-Bowman <72958677+The-Bowman@users.noreply.github.com>
2020-11-11 22:27:21 -0700

    added author name

--- CalorieCounterConverter/Form1.cs
+++ CalorieCounterConverter/Form1.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*
+ * author: Dustin Johnson
+ */
+
 namespace CalorieCounterConverter
 {
     public partial class fatsCarbsConverterForm : Form
